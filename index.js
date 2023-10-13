e0b47c982e688a1a15623e42424ebcf9134246f4
DCKAP <santhoshkumar.edckap@gmail.com>
2023-10-13 10:31:57 +0530

    JS-problems-1 add

---
+++ index.js
@@ -0,0 +1,69 @@
+"use strict"
+
+// --------------sum1---------------------
+
+// let arr=([1,2,"13","4","645",2,3])
+// let b=0
+// for(let i=0;i<arr.length;i++)
+// {
+//     if (typeof(arr[i])=="number"){
+//         b+=arr[i]
+//     }
+// }
+// console.log(b)
+
+// ----------------sum2------------------
+
+// let arr=([10,40,45,30,20,50])
+
+// for(i=0;i<arr.length;i++)
+// {
+//     for(j=i+1;j<arr.length;j++)
+//     {
+//         if (arr[i]>arr[j]){
+//             temp=arr[i]
+//             arr[i]=arr[j]
+//             arr[j]=temp
+//         }
+//     } 
+// }
+// var val=arr.length-2
+// console.log(arr[val])
+
+// -----------------sum4------------------
+
+// let arr=input([5,2,1,7,3,4],"none")
+// var x
+
+// function input(arr,order) {
+//  if(order=="asc")   
+
+// for(i=0;i<arr.length;i++)
+// {
+//     for(j=0;j<arr.length;j++){
+//         if(arr[i]<arr[j])
+//         {
+//             x=arr[i]
+//             arr[i]=arr[j]
+//             arr[j]=x
+//         }
+//     }
+// }
+// else if(order=="des"){
+//     for(i=0;i<arr.length;i++)
+// {
+//     for(j=0;j<arr.length;j++){
+//         if(arr[i]>arr[j])
+//         {
+//             x=arr[i]
+//             arr[i]=arr[j]
+//             arr[j]=x
+//         }
+// }
+// }
+// }
+// console.log(arr)
+// }
+
+// --------------------------------------
+
